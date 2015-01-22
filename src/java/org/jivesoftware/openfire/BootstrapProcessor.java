4fcbb0aaacbe9b0cfe1f2aeae0fb92fa4b6c53ad
Sujay Datar <sujay.datar@magnet.com>
2015-01-22 10:12:46 -0800

    More bootstrap file

---
+++ src/java/org/jivesoftware/openfire/BootstrapProcessor.java
@@ -0,0 +1,14 @@
+package org.jivesoftware.openfire;
+
+public class BootstrapProcessor {
+	private BootstrapProperties bootstrapProps;
+
+	public BootstrapProcessor(BootstrapProperties bootstrapProps) {
+		this.bootstrapProps = bootstrapProps;
+	}
+
+	public void bootstrap() {
+		
+	}
+	
+}
