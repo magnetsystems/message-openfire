5b3a512d09604f646d3dd7ea764c6a6b5dfe9382
Helen Chiang Bragg <helen.bragg@magnet.com>
2015-05-22 16:57:46 -0700

    Add mvn install shell script for local repository

---
+++ mvn_install_local.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Deploy openfire.jar to local maven repository
+CURDIR=`pwd`
+ARTIFACT_VERSION="1.0.2-SNAPSHOT"
+FILE="${CURDIR}/target/openfire/lib/openfire.jar"
+
+cd ${CURDIR}/target
+zip -r mmx-openfire.zip openfire
+FILE_ZIP="${CURDIR}/target/mmx-openfire.zip"
+
+mvn \
+  -Dfile=$FILE \
+  -DgroupId=com.magnet.mmx.ext \
+  -DartifactId=openfire \
+  -Dversion=$ARTIFACT_VERSION \
+  -Dpackaging=jar \
+  -DgeneratePom=true \
+  install:install-file
+
+mvn \
+  -Dfile=$FILE_ZIP \
+  -DgroupId=com.magnet.mmx.ext \
+  -DartifactId=mmx-openfire \
+  -Dversion=$ARTIFACT_VERSION \
+  -Dpackaging=zip \
+  -DgeneratePom=true \
+  install:install-file
