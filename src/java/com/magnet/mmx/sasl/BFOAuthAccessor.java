4c89a6b63291b856fe809d2208f2e3e33fedde84
Vincent Lau <vincent.lau@magnet.com>
2015-10-19 19:25:05 -0700

    Remove unused import.

--- src/java/com/magnet/mmx/sasl/BFOAuthAccessor.java
+++ src/java/com/magnet/mmx/sasl/BFOAuthAccessor.java
@@ -16,7 +16,6 @@ package com.magnet.mmx.sasl;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
